016e1ba8850e8d96ffa79b9862a7e56f01c05c8c
mishafromboyz <48093530+mishafromboyz@users.noreply.github.com>
2021-01-24 21:15:46 +0700

    Update test.c

--- test/test.c
+++ test/test.c
@@ -49,32 +49,3 @@ CTEST(lib, copyFile)
         ASSERT_EQUAL(exp1, res);
     }
 }
-/*
-CTEST(lib, showsize)
-{
-    int exp1 = 6;
-    int exp2 = 2;
-    int res = showsize("/home/diana/ycheba/os/LiteSh/test/8.txt");
-
-    ASSERT_EQUAL(exp1, res);
-    ASSERT_NOT_EQUAL(exp2, res);
-
-}
-/*
-/*
-CTEST(lib, show_all_files)
-{
-    DIR *dir = opendir("/home/diana/ycheba/os/LiteSh/test/testsh");
-    char *exp1 = "\n7.txt\n..\n."; 
-    char *exp2 = "-1";
-    char *res;
-
-    res = show_all_files("/home/diana/ycheba/os/LiteSh/test/testsh");
-
-    if (dir == NULL) {
-        ASSERT_STR(exp2, res);
-    } else {
-        ASSERT_STR(exp1, res);
-    }
-}
-*/
